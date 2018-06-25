86129aae4415385d5e2ef3d49df0d3a4b6933258
hbchen <k160438@sjtu.edu.cn>
2018-06-25 17:12:18 +0800

    add 5 effects

--- my_app/js/IconLayout.js
+++ my_app/js/IconLayout.js
@@ -1,10 +1,10 @@
-String.prototype.format = function (args) {
+String.prototype.format = function(args) {
     var result = this;
     if (arguments.length < 1) {
         return result;
     }
     var data = arguments;
-    if (arguments.length == 1 && typeof (args) == "object") {
+    if (arguments.length == 1 && typeof(args) == "object") {
         data = args;
     }
     for (var key in data) {
@@ -18,7 +18,7 @@ String.prototype.format = function (args) {
 appUl = document.getElementById('app');
 
 const fs = require('fs');
-fs.readFile('my_app/iconList.txt', function (err, data) {
+fs.readFile('my_app/iconList.txt', function(err, data) {
     if (err) {
         return console.error(err);
     }
@@ -34,19 +34,23 @@ fs.readFile('my_app/iconList.txt', function (err, data) {
 
 function displayOneIcon(IconStr) {
     var div = document.createElement("div");
-    div.className = "icon";
+    div.className = "icon hover"; //原来为 icon
     var arr = IconStr.split('\t');
     var appName = arr[0].split('.lnk')[0].split('.url')[0];
     // var htmlStr ='<a href = "#" id = "{a}" > <img src = "images/icons/{b}.jpg" >  <br /> {c} </a>';
     // htmlStr = htmlStr.format({ 'a': appName, 'b': arr[0], 'c': appName });
 
+<<<<<<< HEAD
+    var htmlStr = '<a href = "#" id = "{a}" > <img src = "images/icons/{b}.jpg" >  <br /> {c} </a>';
+    htmlStr = htmlStr.format({ 'a': arr[0], 'b': arr[0], 'c': arr[0] });
+
+=======
     var htmlStr = '<a href = "#" id = "{a}"> <div> <figure style="background: url(images/icons/{b}.jpg); background-size: 100%;" > <figcaption> {c} </figcaption> </figure > </div > </a>';
     const reg = new RegExp(' ', "g")
     htmlStr = htmlStr.format({ 'a': appName, 'b': arr[0].replace(reg, '%20'), 'c': appName });
     
+>>>>>>> 0d88e217b675b07bd723e61955535c4dbe7b5fae
     div.innerHTML = htmlStr;
     return div;
-    
+
 }
-
-
