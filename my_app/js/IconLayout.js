0cef590e5358fb7d6ffbd2385c4cbeb647bca91e
noxic <jhxxszy@hotmail.com>
2018-06-25 21:26:03 +0800

    Add dynamic icon effect changer

--- my_app/js/IconLayout.js
+++ my_app/js/IconLayout.js
@@ -39,17 +39,24 @@ function displayOneIcon(IconStr) {
     div.className = "icon";
     var arr = IconStr.split('\t');
     var appName = arr[0].split('.lnk')[0].split('.url')[0];
-    
+    var htmlStr;
 
-    // chenhongbin
-    var htmlStr ='<a href = "#" class="origin" id = "{a}" > <img src = "images/icons/{b}.jpg" >  <br /> {c} </a>';
-    htmlStr = htmlStr.format({ 'a': appName, 'b': arr[0], 'c': appName });
+    var IconStyle = fs.readFileSync('my_app/Style.txt', 'utf-8');
+    IconStyle = Number(IconStyle);
+    if (IconStyle == 1) // Domino
+    {
+            
+        htmlStr = '<a href = "#" id = "{a}"> <div> <figure style="background: url(images/icons/{b}.jpg); background-size: 100%;" > <figcaption> {c} </figcaption> </figure > </div > </a>';
+        const reg = new RegExp(' ', "g")
+        htmlStr = htmlStr.format({ 'a': appName, 'b': arr[0].replace(reg, '%20'), 'c': appName });
+    }
+    else if (IconStyle < 5) // chenhongbin
+    {
+        htmlStr = '<a href = "#" class="origin" id = "{a}" > <img src = "images/icons/{b}.jpg" >  <br /> {c} </a>';
+        htmlStr = htmlStr.format({ 'a': appName, 'b': arr[0], 'c': appName });
+    }
 
-    // // Domino
-    // var htmlStr = '<a href = "#" id = "{a}"> <div> <figure style="background: url(images/icons/{b}.jpg); background-size: 100%;" > <figcaption> {c} </figcaption> </figure > </div > </a>';
-    // const reg = new RegExp(' ', "g")
-    // htmlStr = htmlStr.format({ 'a': appName, 'b': arr[0].replace(reg, '%20'), 'c': appName });
-    
+
     div.innerHTML = htmlStr;
     return div;
     
